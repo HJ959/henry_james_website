4bde97aa25d268cd767f7cdd5c35fca2e0df8ec0
HJ959 <henryjaames@gmail.com>
2020-04-02 20:40:15 +0100

    Simplified how the display sections functionality works

--- js/myFunctions.js
+++ js/myFunctions.js
@@ -31,66 +31,30 @@ function getRandomInt(min, max) {
 }
 //////////////////////////////////////////////////////////////////////////////
 function showAV() {
-  window.location = "#audioVisual";
-  displaySection("js-audio-visual-section");
+  displaySection("js-audio-visual-section", "#audioVisual");
 }
 //////////////////////////////////////////////////////////////////////////////
 function showHome() {
-  window.location = "";
-  displaySection("js-home-section");
+  displaySection("js-home-section", "#");
 }
 //////////////////////////////////////////////////////////////////////////////
 function showPrints() {
-  window.location = "#vibrant";
-  displaySection("js-prints-section");
+  displaySection("js-prints-section", "#vibrant");
 }
-
-// DRY Principle
-// Back Button = Shows Home - Hides Everything Else
-// Visual Button = Shows Visual - Hides Everything Else
-// Prints Button = Shows Prints - Hides Everything Else
-
-// A button that hides all content sections then shows element.
-// 
-function displaySection(elmnt) {
+//////////////////////////////////////////////////////////////////////////////
+function displaySection(elmnt, urlHash) {
+  window.location.hash = urlHash;
   elmnt = document.getElementById(elmnt);
   sections = document.getElementsByClassName('content-section');
-  console.log(sections);
 
-  for (var section; section > sections.length; sections++) {
-     console.log('section', section);
-     
-     sections[section].classList.remove('is-showing');
+  for (var i = 0; i < sections.length; i++) {  
+     console.log('section', sections[i]);
+     sections[i].classList.remove('is-showing');
+     console.log('section', sections[i]);
   }
   elmnt.classList.add('is-showing');
 }
 //////////////////////////////////////////////////////////////////////////////
-function showHide(x, y, blockType, hashString) {
-
-  if (x.style.display === "none") {
-      x.style.display = blockType;
-      x.visibility = "visible";
-      window.location = "";
-    } else {
-      x.style.display = "none";
-      x.visibility = "hidden";
-    }
-
-    if (x.style.display === blockType) {
-      y.style.display = "none";
-      y.visibility = "hidden";
-    } else {
-      y.style.display = blockType;
-      y.visibility = "visible";
-      // update the hash values so that URLs can be shared with viewers
-      window.location = hashString;
-    }
-
-
-    // 
-}
-//////////////////////////////////////////////////////////////////////////////
-// displays the selected div on the catalogue screen
 function returnCatInfo(buttonValue) {
   var printIDList = ['print1', 'print2', 'print3', 'print4', 'print5', 'print6', 'print7'];
   for (item in printIDList) {
@@ -137,29 +101,3 @@ function printsPrevious() {
   previousBtn("nextButton");
 }
 //////////////////////////////////////////////////////////////////////////////
-function isImgMaxSize(imgID) {
-  // gets the original image size and current and makes sure
-  // the image doesn't go over this to stop poor resolution
-  var img = document.getElementById(imgID);
-
-  actualHeight = img.naturalHeight;
-  actualWidth = img.naturalWidth;
-  currentHeight = img.height;
-  currentWidth = img.width;
-}
-//////////////////////////////////////////////////////////////////////////////
-
-// When we navigate to a section, we want to update the URL to include a hash value that signifies what section we are on
-// Helpful helpers that you might need are:
-// To set the hash initially, you will need: window.location = "#hash-goes-here"
-// To update the hash, you will need: window.location.hash = "#new-hash"
-
-// #section#two
-
-// When we land on the website, we want to check to see if there is a hash value appended to the URL, and if there is, we want to show the corresponding section.
-// To fetch the hash, you will need window.location, and maybe a split of the string on the hash. i.e. window.location.split('#');
-// split
-// window.location
-// window.location.hash
-
-// var newString = window.location.split('#');
