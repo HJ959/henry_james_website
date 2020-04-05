873bb54017c52812cbc6cc4c8738992a75dc2d31
HJ959 <henryjaames@gmail.com>
2020-04-05 13:59:23 +0100

    Added new piece with T.S. Idiot

--- js/zdog-script.js
+++ js/zdog-script.js
@@ -1,110 +1,163 @@
-// zdog-demo.js
+/*
+Created by Henry James for collaboration with T.S. Idiot,
+Created: 04/04/2020
+Poems by T.S. Idiot
+Visuals and website made by Henry James 
+*/
+
 // rotating flag variable
 let isSpinning = true;
 // create illo
 let illo = new Zdog.Illustration({
-    // set canvas with selector
-    element: '.zdog-svg',
-    // enable rotating scene with dragging
-    dragRotate: true,
-    onDragMove: function() {
-      isSpinning = false;
-    },
-    onDragEnd: function() {
-      isSpinning = true;
-    },
-  });
-  var counter = 0;
+	// set canvas with selector
+	element: '.zdog-svg',
+	// enable rotating scene with dragging
+	dragRotate: true,
+	onDragMove: function() {
+		isSpinning = false;
+	},
+	onDragEnd: function() {
+		isSpinning = true;
+	},
+});
 
+
+var counter = 0;
 var synth_names = ['synth_1', 'synth_2', 'synth_3', 'synth_4'];
 var synths = {
-  synth_1: new Howl({
-    src: ['media/Ts_idiot_Collab/synth_1.mp3'],
-    volume: 1.0,
-    onend: function() {
-      synths[synth_names[getRandomInt(0,5)]].play();
-    }}),
-  synth_2: new Howl({
-    src: ['media/Ts_idiot_Collab/synth_2.mp3'],
-    volume: 1.0,
-    onend: function() {
-      synths[synth_names[getRandomInt(0,5)]].play();
-    }}),
-  synth_3: new Howl({
-    src: ['media/Ts_idiot_Collab/synth_3.mp3'],
-    volume: 1.0,
-    onend: function() {
-      synths[synth_names[getRandomInt(0,5)]].play();
-    }}),
-  synth_4: new Howl({
-    src: ['media/Ts_idiot_Collab/synth_4.mp3'],
-    volume: 1.0,
-    onend: function() {
-      synths[synth_names[getRandomInt(0,5)]].play();
-    }}),
+	synth_1: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_1.mp3'],
+		volume: 1.0,
+		onend: function() {
+			synths[synth_names[getRandomInt(0, 5)]].play();
+		}
+	}),
+	synth_2: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_2.mp3'],
+		volume: 1.0,
+		onend: function() {
+			synths[synth_names[getRandomInt(0, 5)]].play();
+		}
+	}),
+	synth_3: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_3.mp3'],
+		volume: 1.0,
+		onend: function() {
+			synths[synth_names[getRandomInt(0, 5)]].play();
+		}
+	}),
+	synth_4: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_4.mp3'],
+		volume: 1.0,
+		onend: function() {
+			synths[synth_names[getRandomInt(0, 5)]].play();
+		}
+	}),
 }
-/*
-  var synths = new Howl({
-    src: ['media/Ts_idiot_Collab/synth_4.mp3', 
-          'media/Ts_idiot_Collab/synth_3.mp3', 
-          'media/Ts_idiot_Collab/synth_2.mp3'],
-    autoplay: true,
-    loop: true,
-    volume: 1.0,
-    onend: function() {
-      console.log('Finished!');
-    }
-  });*/
 
-  var robos = new Howl({
-    src: ['media/Ts_idiot_Collab/Isambard.mp3', 
-          'media/Ts_idiot_Collab/Mean_girls.mp3', 
-          'media/Ts_idiot_Collab/Netflic_and_chill.mp3'],
-    volume: 1.0,
-    onend: function() {
-      counter = 0;
-    }
-  });
-  
-  synths.synth_1.play();
- 
 
-  var rand = 0;
-  let polyOne, polyTwo;
-  
-  var colours = ['#D8F9F7','#D8EBF9','#D8F9E7'];
-  var true_false = [true, false];
-  function animate() {
-    // rotate illo each frame
-      // add circle
-      
-      if (counter < 5) {
-        counter += 1;
-    polyOne = new Zdog.Shape({
-      addTo: illo,
-      path: [
-        { x: getRandomInt(-120,120), y: getRandomInt(-120,120), z: getRandomInt(-120,120) },   // start
-        { arc: [
-          { x: getRandomInt(-120,120), y: getRandomInt(-120,120), z: getRandomInt(-120,120) }, // corner
-          { x: getRandomInt(-120,120), y: getRandomInt(-120,120), z: getRandomInt(-120,120) }, // end point
-        ]},
-        { arc: [ // start next arc from last end point
-          { x: getRandomInt(-120,120), y: getRandomInt(-120,120), z: getRandomInt(-120,120) }, // corner
-          { x: getRandomInt(-120,120), y: getRandomInt(-120,120), z: getRandomInt(-120,120) }, // end point
-        ]},
-      ],
-      closed: true_false[getRandomInt(0,2)],
-      stroke: getRandomInt(0,4),
-      color: colours[getRandomInt(0,3)],
-    });
-  } 
+var robo_names = ['isambard', 'mean_girls', 'netflic_and_chill', 'personal_pollution', 'pink_jeans'];
+var robos = {
+	isambard: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_1.mp3'],
+		volume: 1.0,
+		onend: function() {
+			robos[robo_names[getRandomInt(0, 6)]].play();
+			counter = 0;
+		}
+	}),
+	mean_girls: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_2.mp3'],
+		volume: 1.0,
+		onend: function() {
+			robos[robo_names[getRandomInt(0, 6)]].play();
+			counter = 0;
+		}
+	}),
+	netflic_and_chill: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_3.mp3'],
+		volume: 1.0,
+		onend: function() {
+			robos[robo_names[getRandomInt(0, 6)]].play();
+			counter = 0;
+		}
+	}),
+	personal_pollution: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_4.mp3'],
+		volume: 1.0,
+		onend: function() {
+			robos[robo_names[getRandomInt(0, 6)]].play();
+			counter = 0;
+		}
+	}),
+	pink_jeans: new Howl({
+		src: ['media/Ts_idiot_Collab/synth_4.mp3'],
+		volume: 1.0,
+		onend: function() {
+			robos[robo_names[getRandomInt(0, 6)]].play();
+			counter = 0;
+		}
+	}),
+}
 
-    if ( isSpinning ) {
-      illo.rotate.y += 0.01;
-    } 
-    illo.updateRenderGraph();
-    // animate next frame   
-    requestAnimationFrame( animate );
-  }
-  // start animation
-  animate();
+synths.synth_1.play();
+robos.isambard.play();
+
+var rand = 0;
+let shape;
+var colours = ['#D8F9F7', '#D8EBF9', '#D8F9E7'];
+var true_false = [true, false];
+
+function animate() {
+	// rotate illo each frame
+	// add circle
+	if (counter < 5) {
+		counter += 1;
+		shape = new Zdog.Shape({
+			addTo: illo,
+			path: [{
+					x: getRandomInt(-120, 120),
+					y: getRandomInt(-120, 120),
+					z: getRandomInt(-120, 120)
+				}, // start
+				{
+					arc: [{
+							x: getRandomInt(-120, 120),
+							y: getRandomInt(-120, 120),
+							z: getRandomInt(-120, 120)
+						}, // corner
+						{
+							x: getRandomInt(-120, 120),
+							y: getRandomInt(-120, 120),
+							z: getRandomInt(-120, 120)
+						}, // end point
+					]
+				}, {
+					arc: [ // start next arc from last end point
+						{
+							x: getRandomInt(-120, 120),
+							y: getRandomInt(-120, 120),
+							z: getRandomInt(-120, 120)
+						}, // corner
+						{
+							x: getRandomInt(-120, 120),
+							y: getRandomInt(-120, 120),
+							z: getRandomInt(-120, 120)
+						}, // end point
+					]
+				},
+			],
+			closed: true_false[getRandomInt(0, 2)],
+			stroke: getRandomInt(0, 4),
+			color: colours[getRandomInt(0, 3)],
+		});
+	}
+	if (isSpinning) {
+		illo.rotate.y += 0.01;
+	}
+	illo.updateRenderGraph();
+	// animate next frame   
+	requestAnimationFrame(animate);
+}
+// start animation
+animate();
