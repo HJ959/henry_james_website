ce5b8a31417c884981242b607552a705143d5d09
HJ959 <henryjaames@gmail.com>
2020-04-15 19:22:22 +0100

    Begun using gulp to compile SASS and do other various things with optimisation

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
+
+gulp.task('styles', () => {
+    return gulp.src('sass/main.scss')
+        .pipe(plumber())
+        .pipe(sass({includePaths: ['./styles']}))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 3 versions'))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);  
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
