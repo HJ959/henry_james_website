fe0e0ce647201445e8710ebf1e57e7592f457489
HJ959 <henryjaames@gmail.com>
2020-04-19 12:50:00 +0100

    minified CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,28 @@ const sass = require('gulp-sass');
 const del = require('del');
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
 
+/*
 // add babel sorts out your new js code to work with old code
-
+gulp.task('js', () =>
+    gulp.src('js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('dist/js'))
+);
+*/
 gulp.task('styles', () => {
     return gulp.src('sass/main.scss')
         .pipe(plumber())
         .pipe(sass({includePaths: ['./styles']}))
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 3 versions'))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./css/'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('clean', () => {
