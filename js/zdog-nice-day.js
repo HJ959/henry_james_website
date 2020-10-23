a4572d40a5aa604c5bb000f7c78a2e623fea3af0
HJ959 <henryjaames@gmail.com>
2020-10-23 10:33:24 +0100

    Working on the new itsgonnabeokay piece

---
+++ js/zdog-nice-day.js
@@ -0,0 +1,29 @@
+// Initialize Zfont
+Zfont.init(Zdog);
+
+// Create a Zdog illustration
+let illo = new Zdog.Illustration({
+    element: '.zdog-svg',
+});
+
+// Set up a font to use
+let myFont = new Zdog.Font({
+  src: 'media/font/Staatliches-Regular.ttf'
+});
+
+// Create a text object
+// This is just a Zdog.Shape object with a couple of extra parameters!
+new Zdog.Text({
+  addTo: illo,
+  font: myFont,
+  value: 'Hey, Zdog!',
+  fontSize: 64,
+  color: '#fff'
+});
+
+// Animation loop
+function animate() {
+  illo.updateRenderGraph();
+  requestAnimationFrame(animate);
+}
+animate();
