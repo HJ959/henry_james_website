1185314809e70ceb402da9893ad76095bbbbbaa1
HJ959 <henryjaames@gmail.com>
2020-10-23 10:36:54 +0100

    Working on the new itsgonnabeokay piece

--- js/zdog-nice-day.js
+++ js/zdog-nice-day.js
@@ -4,12 +4,21 @@ Zfont.init(Zdog);
 // Create a Zdog illustration
 let illo = new Zdog.Illustration({
     element: '.zdog-svg',
+    // enable rotating scene with dragging
+	dragRotate: true,
+	onDragMove: function() {
+		isSpinning = false;
+	},
+	onDragEnd: function() {
+		isSpinning = true;
 });
 
 // Set up a font to use
 let myFont = new Zdog.Font({
   src: './media/font/Staatliches-Regular.ttf'
 });
+
+var colours = ['#8CFAEB', '#8CD3FA', '#8CFAB4'];
 
 // Create a text object
 // This is just a Zdog.Shape object with a couple of extra parameters!
@@ -18,7 +27,7 @@ new Zdog.Text({
   font: myFont,
   value: 'Hey, Zdog!',
   fontSize: 64,
-  color: '#fff'
+  color: colours[getRandomInt(0, 2)]
 });
 
 // Animation loop
