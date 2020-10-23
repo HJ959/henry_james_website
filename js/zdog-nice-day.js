7fe839ac8ba465caa52e3bce50dd0b96651730c7
HJ959 <henryjaames@gmail.com>
2020-10-23 10:43:30 +0100

    Working on the new itsgonnabeokay piece

--- js/zdog-nice-day.js
+++ js/zdog-nice-day.js
@@ -20,19 +20,24 @@ let myFont = new Zdog.Font({
 });
 
 var colours = ['#8CFAEB', '#8CD3FA', '#8CFAB4'];
+var sentences = ["It's gonna be okay.", "Chin up m8", "Deep breathes",
+                 "Just relax.", "There's always a last time.", "There's always a first time.",
+                 "Think of all the things you're grateful for."];
+var max_sentences = length(sentences);
 
 // Create a text object
 // This is just a Zdog.Shape object with a couple of extra parameters!
 new Zdog.Text({
   addTo: illo,
   font: myFont,
-  value: 'Hey, Zdog!',
+  value: sentences[getRandomInt(0, max_sentences)],
   fontSize: 64,
   color: colours[getRandomInt(0, 2)]
 });
 
 // Animation loop
 function animate() {
+  illo.rotate.y += 0.01;
   illo.updateRenderGraph();
   requestAnimationFrame(animate);
 }
