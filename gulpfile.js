5c0c6f616d81c6d573b32636e88d2f2d344e2fda
HJ959 <henryjaames@gmail.com>
2020-10-31 09:09:40 +0000

    Updated itsgonnabeokay.html with new zDog drawings

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass');
 const del = require('del');
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber');
+var connect = require('gulp-connect');
 
 // add babel sorts out your new js code to work with old code
 
@@ -21,4 +22,9 @@ gulp.task('clean', () => {
     ]);  
 });
 
-gulp.task('default', gulp.series(['clean', 'styles']));
+gulp.task('webserver', function() {
+    connect.server({livereload: true});
+  });
+
+
+gulp.task('default', gulp.series(['clean', 'styles', 'webserver']));
--- js/zdog-nice-day.js
+++ js/zdog-nice-day.js
@@ -6,12 +6,9 @@ let illo = new Zdog.Illustration({
     element: '.zdog-svg',
     // enable rotating scene with dragging
 	dragRotate: true,
-	onDragMove: function() {
+	onDragStart: function() {
 		isSpinning = false;
-	},
-	onDragEnd: function() {
-        isSpinning = true;
-    },
+	}
 });
 
 // Set up a font to use
@@ -32,12 +29,67 @@ new Zdog.Text({
   font: myFont,
   value: sentences[getRandomInt(0, max_sentences)],
   fontSize: 32,
-  color: colours[getRandomInt(0, 2)]
+  stroke: 3,
+  color: '#FF9393'
 });
+
+// square
+new Zdog.Rect({
+  addTo: illo,
+  width: getRandomInt(60,240),
+  height: getRandomInt(60,240),
+  translate: { z: -getRandomInt(20,40) },
+  stroke: getRandomInt(3,12),
+  color: colours[getRandomInt(0,2)],
+  fill: false,
+});
+
+function draw(input_x, input_y, width, height) {
+  var leftToRight = Math.random() >= 0.5;
+  
+  if(leftToRight) {
+    new Zdog.Shape({
+      addTo: illo,
+      path: [
+        { x: input_x, y: input_y, z: getRandomInt(-2,2) }, 
+        { x:  input_x + width, y: input_y + height, z: getRandomInt(-1,2) }, 
+      ],
+      translate: { y: 10, z: -10},
+      closed: false,
+      stroke: getRandomInt(1,3),
+      color: colours[getRandomInt(0,2)],
+      });
+  } else {
+    new Zdog.Shape({
+      addTo: illo,
+      path: [
+        { x: input_x + width, y: input_y}, 
+        { x:  input_x, y: input_y + height, z: getRandomInt(-1,1) }, 
+      ],
+      translate: { y: 10, z: -10},
+      closed: false,
+      stroke: getRandomInt(1,3),
+      color: colours[getRandomInt(0,2)],
+      });
+  }
+};
+
+var svg_canvas   = document.getElementById("itsgonnabeokay-svg"); // or other selector like querySelector()
+var rect = svg_canvas.getBoundingClientRect(); // get the bounding rectangle
+
+var size = getRandomInt(50,100);
+var step = Math.round(size / 10);
+var dpr = window.devicePixelRatio;
+
+for(var x = 0; x < size; x += step) {
+  for(var y = 0; y < size; y+= step) {
+    draw(x, y, step, step);    
+  }
+}
 
 // Animation loop
 function animate() {
-  illo.rotate.y += 0.01;
+
   illo.updateRenderGraph();
   requestAnimationFrame(animate);
 }
